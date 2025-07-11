3d9f79aaf9e4b3cf8317f7f91abdec0b344db3ed
professorpoof <nappysan1@gmail.com>
2025-07-11 02:18:05 -0600

    Poof hooks rebel_rev_vroom.mp3 into clicker logic

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const counter = document.getElementById("counter");
 const samwellImg = document.querySelector(".pixel-scene img");
 
+// load the click sound
+const clickSound = new Audio("assets/sounds/rebel_rev_vroom.mp3");
+
 samwellImg.style.cursor = "pointer";
 
 samwellImg.addEventListener("click", () => {
